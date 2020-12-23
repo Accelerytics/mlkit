b95fb7a510c95907301733d8a8de36ba4269e392
Mathijs995 <mathijsdejong1995@gmail.com>
2020-12-23 17:21:21 +0100

    Simplified implementation of LogitBoost

--- R/logit.boost.R
+++ R/logit.boost.R
@@ -1,4 +1,4 @@
-#' Implementation of the Multiclass LogitBoost Algorithm
+#' Implementation of the Binary LogitBoost Algorithm
 #'
 #' @description Implementation of the LogitBoost algorithm based on
 #' Friedman, J., Hastie, T., and Tibshirani, R. (2000)
@@ -11,7 +11,6 @@
 #' variables in the model. If not found in data, the variables are taken from
 #' environment (\code{formula}), typically the environment from which this
 #' function is called.
-#'
 #' @param n.iter number of iterations to apply.
 #'
 #' @return \code{logic.boost} returns an object of \code{\link{class}}
@@ -25,46 +24,32 @@
 logit.boost = function(formula, data, n.iter) {
 
   # Extract dependent variable and explanatory variables
-  dep.var = all.vars(formula)[1]
-  y = as.factor(df$train[[dep.var]])
-  y.star = stats::model.matrix(~ 0 + ., as.data.frame(y))
-  x = cbind(1, stats::model.matrix(formula, data))
-
-  # Ensure y is correctly formatted and non-singular
-  n.obs = nrow(y.star); n.class = ncol(y.star)
-  if (n.class == 1 | n.class == n.obs)
-    stop("Ensure dependent variable contains between 1 and n.obs categories.")
+  y = data[[all.vars(formula)[1]]]; n.obs = length(y)
+  x = stats::model.matrix(formula, data)
 
   # Step 1: Initialize variables
-  w = matrix(1 / n.obs, nrow=n.obs, ncol=n.class); f = list()
-  z = matrix(nrow=n.obs, ncol=n.class); upt.w = (n.class - 1) / n.class
-  p = matrix(1 / n.class, nrow=n.obs, ncol=n.class)
+  f = matrix(0, nrow=n.iter, ncol=ncol(x)); p = rep(0.5, n.obs)
 
   # Step 2: Apply n.iter updates to find MLE
   for (m in 1:n.iter) {
 
     # a.i: Compute working responses and weights in all classes
-    w = p * (1 - p); z = (y.star - p) / w
+    w = pmax(p * (1 - p), 2 * .Machine$double.xmin)
+    z = pmax(pmin(ifelse(y == 1, 1 / p, - 1 / (1 - p)), 4), -4)
 
     # a.ii: Fit WLS regression
-    f[[m]] = lapply(1:n.class,
-      function(j) glm.fit(x=x, y=z[, j], weights=w[, j])$coefficients)
+    f[m, ] = glm.fit(x=x, y=z, weights=w)$coefficients
 
     # b: Boosting update
-    f[[m]] = lapply(1:n.class,
-      function(j) upt.w * (f[[m]][[j]] - Reduce('+', f[[m]]) / n.class))
-    f.func = function(x) sapply(
-      1:n.class,
-      function(j) x %*% Reduce('+', lapply(1:m, function(k) f[[k]][[j]]))
-    )
+    f.func = function(x) 0.5 * x %*% colSums(f)
 
     # c: Update probabilities
-    tmp = exp(f.func(x)); p = tmp / sum(tmp)
+    f.eval = f.func(x)
+    p = exp(f.eval) / (exp(f.eval) + exp(-f.eval))
   }
 
   # Step 3: Construct classifier function to return
-  classes = unique(y)
-  classifier = function(x) classes[apply(f.func(x), 1, which.max)]
+  classifier = function(x) sign(x %*% colSums(f))
 
   # Return mlfit.class.fit object
   res = list(classifier=classifier, y.hat=classifier(x))
